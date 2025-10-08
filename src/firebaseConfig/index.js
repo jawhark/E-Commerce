9962abea9cf1923b70523590eb6fba823267368e
Jawhar <jawharkoonbhani@gmail.com>
2025-10-08 17:57:07 +0500

    Update index.js

--- src/firebaseConfig/index.js
+++ src/firebaseConfig/index.js
@@ -6,12 +6,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDoVgLfFKKQh6qMHrmGtqoDAIbTAnX5IIA",
-    authDomain: "e-commerce-5e1bc.firebaseapp.com",
-    projectId: "e-commerce-5e1bc",
-    storageBucket: "e-commerce-5e1bc.firebasestorage.app",
-    messagingSenderId: "299686388305",
-    appId: "1:299686388305:web:48d6a472d5043a584d8e70"
+    
 };
 
 // Initialize Firebase
